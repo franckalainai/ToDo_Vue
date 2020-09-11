1bc4fab391c8a733945eec81957252beb4f26532
franckalainai <franckalain.ai@gmail.com>
2020-09-11 17:27:58 +0000

    todo

---
+++ resources/assets/js/components/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="app">
+        Component
+
+    </div>
+</template>
+
+
+<script>
+
+    export default{
+
+        data(){
+
+            return{
+                isLoading: false,
+                tasks: [],
+                task: {title: '', priority: ''}
+
+            }
+
+        },
+
+</script>
+
+
+<style>
+</style>
